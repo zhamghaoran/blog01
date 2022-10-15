3b46fb9580233a06d95a700fb496ab32696a8370
zhanghaoran <2017929959@qq.com>
2022-10-15 10:36:15 +0800

    ""

--- src/main/java/com/zhr/blog01/config/WebMVCConfig.java
+++ src/main/java/com/zhr/blog01/config/WebMVCConfig.java
@@ -1,14 +1,24 @@
 package com.zhr.blog01.config;
 
+import com.zhr.blog01.handler.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 跨域配置
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-
     }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/test");
+    }
+
 }
